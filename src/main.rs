dace313dd6bc49d61bef7d2446d4795309861a2f
Nikita Kulagin <kulagin.nikita03@gmail.com>
2024-07-06 21:33:19 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use windows_service::service::{
 use windows_service::service_control_handler::{self, ServiceControlHandlerResult};
 
 use std::ffi::OsString;
+use std::sync::mpsc::{Receiver, TryRecvError};
 use std::sync::{mpsc, Arc};
 use std::time::Instant;
 
@@ -82,10 +83,23 @@ pub fn run_service() -> anyhow::Result<()> {
         process_id: None,
     })?;
 
-    run_tokio()
+    run_tokio(shutdown_rx)?;
+
+     // Tell the system that service has stopped.
+    status_handle.set_service_status(ServiceStatus {
+        service_type: ServiceType::OWN_PROCESS,
+        current_state: ServiceState::Stopped,
+        controls_accepted: ServiceControlAccept::empty(),
+        exit_code: ServiceExitCode::Win32(0),
+        checkpoint: 0,
+        wait_hint: Duration::default(),
+        process_id: None,
+    })?;
+
+    Ok(())
 }
 
-pub fn run_tokio() -> anyhow::Result<()> {
+pub fn run_tokio(shutdown_rx: Receiver<()>) -> anyhow::Result<()> {
     let rt = tokio::runtime::Runtime::new()?;
     rt.block_on(async {
         let mac_list: Vec<MacAddress> = MacAddressIterator::new()?.collect();
@@ -100,66 +114,75 @@ pub fn run_tokio() -> anyhow::Result<()> {
         let mut timeout: Option<JoinHandle<()>> = None;
 
         loop {
-            let (byte_amount, _src_addr) = listener.recv_from(&mut buf).await?;
-            if byte_amount != 102 {
-                continue;
+            match shutdown_rx.try_recv() {
+                Err(TryRecvError::Empty) => (),
+                _ => break,
             }
-            // println!("Received data: {buf:?}");
-
-            let is_wol = buf[0..6].iter().all(|&x| x == 255);
-            if !is_wol {
-                continue;
-            }
-
-            let is_current_device = (6..byte_amount)
-                .step_by(6)
-                .all(|i| mac_list.iter().any(|mac| mac.bytes() == &buf[i..i + 6]));
-
-            if !is_current_device {
-                println!("Missed device");
-                continue;
-            }
-            // println!("Device: {is_current_device}");
-
-            match last_received_time.elapsed().as_millis() >= received_debounce {
-                true => last_received_time = Instant::now(),
-                false => continue,
-            }
-
-            // ====================================
-            // 1. wait == false
-            //    wait = true
-            //    create timeout,
-            //      await => wait = false
-            //
-            // 2. wait == true
-            //    timeout.abort()
-            //    wait = false
-
-            let mut wait = wait_for_sleep.lock().await;
-            if !*wait {
-                *wait = true;
-                let t = Arc::clone(&wait_for_sleep);
-                println!("start wait");
-                timeout = Some(tokio::spawn(async move {
-                    sleep(sleep_delay).await;
-                    let mut wait = t.lock().await;
-                    *wait = false;
-                    println!("sleep");
-
-                    #[cfg(not(debug_assertions))]
-                    suspend();
-                }));
-            } else {
-                if let Some(timeout) = timeout.take() {
-                    timeout.abort();
-                    *wait = false;
-                    println!("abort timeout");
+            if let Ok(byte_amount) = listener.try_recv(&mut buf) {
+                if byte_amount != 102 {
+                    continue;
                 }
+                // println!("Received data: {buf:?}");
+    
+                let is_wol = buf[0..6].iter().all(|&x| x == 255);
+                if !is_wol {
+                    continue;
+                }
+    
+                let is_current_device = (6..byte_amount)
+                    .step_by(6)
+                    .all(|i| mac_list.iter().any(|mac| mac.bytes() == &buf[i..i + 6]));
+    
+                if !is_current_device {
+                    println!("Missed device");
+                    continue;
+                }
+                // println!("Device: {is_current_device}");
+    
+                match last_received_time.elapsed().as_millis() >= received_debounce {
+                    true => last_received_time = Instant::now(),
+                    false => continue,
+                }
+    
+                // ====================================
+                // 1. wait == false
+                //    wait = true
+                //    create timeout,
+                //      await => wait = false
+                //
+                // 2. wait == true
+                //    timeout.abort()
+                //    wait = false
+    
+                let mut wait = wait_for_sleep.lock().await;
+                if !*wait {
+                    *wait = true;
+                    let t = Arc::clone(&wait_for_sleep);
+                    println!("start wait");
+                    timeout = Some(tokio::spawn(async move {
+                        sleep(sleep_delay).await;
+                        let mut wait = t.lock().await;
+                        *wait = false;
+                        println!("sleep");
+    
+                        #[cfg(not(debug_assertions))]
+                        suspend();
+                    }));
+                } else {
+                    if let Some(timeout) = timeout.take() {
+                        timeout.abort();
+                        *wait = false;
+                        println!("abort timeout");
+                    }
+                }
+    
+                println!("wait status: {}", wait);
+            } else {
+                sleep(Duration::from_secs(1)).await;
+                continue;
             }
-
-            println!("wait status: {}", wait);
         }
+        Ok(())
     })
 }
 
